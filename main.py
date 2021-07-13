60bf4609b8b37e1317dd3b5a0779fca7786ae663
Saqifur-Rahman <85213128+Saqifur-Rahman@users.noreply.github.com>
2021-07-13 11:47:25 +0530

    :sparkles: news, events feature added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 from modules.Crypto.crypto import crypto
 from modules.Portfolio.portfolio import portfolio
 
@@ -11,7 +12,15 @@ app.register_blueprint(portfolio, url_prefix="/portfolio")
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    # Events
+    URL = "https://api.coingecko.com/api/v3/events"
+    events = requests.get(url = URL).json()
+
+    API_KEY = "6291b4b15cbb429bba5f669b7d773b00"
+    URL2 = f"https://newsapi.org/v2/everything?q=bitcoin&apiKey={API_KEY}&pageSize=9"
+    news = requests.get(url = URL2).json()
+    
+    return render_template("home.html", events=events, news=news)
 
 if __name__ == "__main__":
     app.run(debug=True)
