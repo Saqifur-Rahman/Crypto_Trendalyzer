6d75ac7295e34a42b3badf73d2a4b68ffd58ce8e
Saqifur-Rahman <85213128+Saqifur-Rahman@users.noreply.github.com>
2021-07-26 21:24:37 +0530

    :beers: portfolio completed

--- modules/Portfolio/portfolio.py
+++ modules/Portfolio/portfolio.py
@@ -1,7 +1,9 @@
 import pyrebase
 import os
+import requests
+import time
 from dotenv import load_dotenv
-from flask import Blueprint, render_template, redirect, session
+from flask import Blueprint, render_template, session
 
 portfolio = Blueprint(
     "portfolio",
@@ -31,12 +33,20 @@ except:
 @portfolio.route("/")
 def my_portfolio():
     if "email" in session:
-        email = session["email"]
-        return render_template("portfolio.html", email=email)
+        email = session['email']
+        portfolio_id = session['portfolio_id']
+        portfolio, portfolio_value = getPortfolio(portfolio_id)
+        transactions, total_invested = getTransactions(email)
+        return render_template("portfolio.html", portfolio=portfolio, transactions=transactions, portfolio_value=portfolio_value, total_invested=total_invested)
     else:
         return render_template("signin.html", status="401")
 
-def getCoinValue(portfolio_id, coin):
+def getCoinDetails(coin):
+    url = f"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids={coin}&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    coin_details = requests.get(url=url).json()
+    return coin_details[0]
+
+def getPortfolio(portfolio_id):
     found=False
     # check if portfolio exists
     for pf in db.child("portfolios").get():
@@ -44,16 +54,49 @@ def getCoinValue(portfolio_id, coin):
             found=True
             break
     if not found:
-        return 0
-    # if exists 
+        return None, 0
+    # if exists
+    coins = []
+    portfolio_value = 0
     portfolios = db.child("portfolios").child(portfolio_id).get()
     for pf in portfolios:
-        if pf.key() == coin:
-            return pf.val()
-    return 0
+        coin_details = getCoinDetails(pf.key())
+        coins.append({
+            "id": str(pf.key()),
+            "units": float(pf.val()),
+            "symbol": coin_details["symbol"],
+            "name": coin_details["name"],
+            "image": coin_details["image"],
+            "current_price": round(coin_details["current_price"],2)
+        })
+        portfolio_value += (coin_details["current_price"]*float(pf.val()))
+    return coins, portfolio_value
+
+def getTransactions(email):
+    transactions=[]
+    total_invested = 0
+    for tsn in db.child("transactions").get():
+        if tsn.val()["email"] == email:
+            tsn_dict = tsn.val()
+            coin_details = getCoinDetails(tsn_dict["coin"])
+            tsn_dict["name"] = coin_details["name"]
+            tsn_dict["symbol"] = coin_details["symbol"]
+            tsn_dict["image"] = coin_details["image"]
+            tsn_dict['time'] = time.ctime(tsn.val()['timestamp'])
+            transactions.append(tsn_dict)
+            if tsn.val()['mode'] == 'BUY':
+                total_invested += tsn.val()["usd"]
+            else:
+                total_invested -= tsn.val()["usd"]
+    if len(transactions) > 0:
+        return transactions, total_invested
+    else:
+        return None, 0
 
 @portfolio.route("/test")
 def test():
-    curr_val = getCoinValue("xyz", "cardano")
+    curr_val, value = getPortfolio("adityakotkar75")
+    # curr_val = getTransactions("adityakotkar75@gmail.com")
+    # curr_val = getCoinDetails("bitcoin")[0]
     # db.child("portfolios").child("xyz").update({"bitcoin": 100})
-    return f"<h1>{curr_val}</h1>"
+    return f"<h1>{curr_val}-{value}</h1>"
